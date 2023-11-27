9ed5f5147cad55d2f1eb42a84c3871b37bd89d5d
iamdevdaniel <maydanachi@gmail.com>
2023-11-27 15:21:45 -0400

    Fix eslint errors and warnings

--- src/components/Form10.tsx
+++ src/components/Form10.tsx
@@ -20,14 +20,14 @@ const Form10: FC<WrappedFormProps> = ({ formData, onInputChange, onSubmit }) => 
     return <Form onSubmit={handleSubmit}>
         <YStack space='$3' width={300} >
             <RadioGroup aria-labelledby='Select one item' name='radio-sex'>
-                <H4>{`Sexo`}</H4>
+                <H4>{'Sexo'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='Macho' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Hembra' />
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-age' onValueChange={handleChange}>
-                <H4>{`Edad`}</H4>
+                <H4>{'Edad'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='Cría' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Juvenil' />
@@ -35,15 +35,15 @@ const Form10: FC<WrappedFormProps> = ({ formData, onInputChange, onSubmit }) => 
                 </YStack>
             </RadioGroup>
             <>
-                <H4>{`Peso Vivo (Kg)`}</H4>
+                <H4>{'Peso Vivo (Kg)'}</H4>
                 <Input size={dimensions.inputSpacing} borderWidth={dimensions.inputBorderWidth} />
             </>
             <>
-                <H4>{`Long. Fibra (cm)`}</H4>
+                <H4>{'Long. Fibra (cm)'}</H4>
                 <Input size={dimensions.inputSpacing} borderWidth={dimensions.inputBorderWidth} />
             </>
             <RadioGroup aria-labelledby='Select one item' name='radio-phisycal-state'>
-                <H4>{`Condición Corporal`}</H4>
+                <H4>{'Condición Corporal'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='Malo' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Regular' />
@@ -51,7 +51,7 @@ const Form10: FC<WrappedFormProps> = ({ formData, onInputChange, onSubmit }) => 
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-pregnancy'>
-                <H4>{`Gestación`}</H4>
+                <H4>{'Gestación'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='No' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Sí' />
@@ -59,42 +59,42 @@ const Form10: FC<WrappedFormProps> = ({ formData, onInputChange, onSubmit }) => 
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-parasite'>
-                <H4>{`Parásitos Externos`}</H4>
+                <H4>{'Parásitos Externos'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='Garrapatas' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Piojos' />
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-parasite'>
-                <H4>{`Sarna`}</H4>
+                <H4>{'Sarna'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='Moderado' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Severo' />
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-dandruff'>
-                <H4>{`Caspa`}</H4>
+                <H4>{'Caspa'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='No' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Sí' />
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-'>
-                <H4>{`Esquila`}</H4>
+                <H4>{'Esquila'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='No' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Sí' />
                 </YStack>
             </RadioGroup>
             <RadioGroup aria-labelledby='Select one item' name='radio-is-dead'>
-                <H4>{`Vicuña Muerta`}</H4>
+                <H4>{'Vicuña Muerta'}</H4>
                 <YStack alignItems={dimensions.radioAlign} space={dimensions.radioSpacing}>
                     <WithRadioGroupItem size={dimensions.radioSize} value='1' label='No' />
                     <WithRadioGroupItem size={dimensions.radioSize} value='2' label='Sí' />
                 </YStack>
             </RadioGroup>
             <>
-                <H4>{`Observaciones`}</H4>
+                <H4>{'Observaciones'}</H4>
                 <TextArea size={dimensions.textAreaSize} borderWidth={dimensions.inputBorderWidth} />
             </>
             <Form.Trigger asChild>
