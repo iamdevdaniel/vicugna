42ac470d3cb5b264b014e63337e6ec408f2aad47
iamdevdaniel <maydanachi@gmail.com>
2023-11-28 14:38:52 -0400

    Isolated checkbox into its own component

--- src/components/Form10.tsx
+++ src/components/Form10.tsx
@@ -13,10 +13,11 @@ import {
     YStack,
 } from 'tamagui'
 import { Check as CheckIcon } from '@tamagui/lucide-icons'
-import { Formik } from 'formik'
+import { Formik, Field, FieldInputProps } from 'formik'
 import { WithCoreForm, RadioGroupItem } from '@components'
 import { WrappedFormProps } from '@models'
 import { dimensions } from '@styles/FormDimensions'
+import CheckboxInput from './CheckboxInput'
 
 const Form10: FC<WrappedFormProps> = ({
     formData,
@@ -27,13 +28,28 @@ const Form10: FC<WrappedFormProps> = ({
         console.log(formValues)
     }
 
-    return <Formik initialValues={{ 'radio-sex': '' }} onSubmit={handleSubmit}>
+    return <Formik
+        initialValues={{
+            'radio-sex': '',
+            'radio-age': '',
+            'text-live-weight': '',
+            'text-fiber-length': '',
+            'radio-phyisycal-state': '',
+            'radio-mange': '',
+            'radio-pregnancy': '',
+            'radio-parasites': '',
+            'chkbx-has-dandruff': null,
+            'chckbx-is-dead': null,
+            'chkbx-can-be-sheared': null,
+        }}
+        onSubmit={handleSubmit
+        }>
         {({ handleChange, handleSubmit }) => (
             <Form onSubmit={handleSubmit}>
                 <YStack space={dimensions.formItemsSpacing} width={300}>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-sex"
+                        aria-labelledby='Select one item'
+                        name='radio-sex'
                         onValueChange={handleChange('radio-sex')}
                     >
                         <H4>{'Sexo'}</H4>
@@ -43,19 +59,19 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="male"
-                                label="Macho"
+                                value='male'
+                                label='Macho'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="female"
-                                label="Hembra"
+                                value='female'
+                                label='Hembra'
                             />
                         </YStack>
                     </RadioGroup>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-age"
+                        aria-labelledby='Select one item'
+                        name='radio-age'
                         onValueChange={handleChange('radio-age')}
                     >
                         <H4>{'Edad'}</H4>
@@ -65,18 +81,18 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="young"
-                                label="Cría"
+                                value='young'
+                                label='Cría'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="juvenile"
-                                label="Juvenil"
+                                value='juvenile'
+                                label='Juvenil'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="adult"
-                                label="Adulto"
+                                value='adult'
+                                label='Adulto'
                             />
                         </YStack>
                     </RadioGroup>
@@ -89,7 +105,7 @@ const Form10: FC<WrappedFormProps> = ({
                         />
                     </>
                     <>
-                        <H4>{'Long. Fibra (cm)'}</H4>
+                        <H4>{'Longitud Fibra (cm)'}</H4>
                         <Input
                             size={dimensions.inputSpacing}
                             borderWidth={dimensions.inputBorderWidth}
@@ -97,9 +113,9 @@ const Form10: FC<WrappedFormProps> = ({
                         />
                     </>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-phisycal-state"
-                        onValueChange={handleChange('radio-phisycal-state')}
+                        aria-labelledby='Select one item'
+                        name='radio-phyisycal-state'
+                        onValueChange={handleChange('radio-phyisycal-state')}
                     >
                         <H4>{'Condición Corporal'}</H4>
                         <YStack
@@ -108,24 +124,24 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="bad"
-                                label="Malo"
+                                value='bad'
+                                label='Malo'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="regular"
-                                label="Regular"
+                                value='regular'
+                                label='Regular'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="good"
-                                label="Bueno"
+                                value='good'
+                                label='Bueno'
                             />
                         </YStack>
                     </RadioGroup>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-pregnancy"
+                        aria-labelledby='Select one item'
+                        name='radio-pregnancy'
                         onValueChange={handleChange('radio-pregnancy')}
                     >
                         <H4>{'Gestación'}</H4>
@@ -135,25 +151,25 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="no"
-                                label="No"
+                                value='no'
+                                label='No'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="yes"
-                                label="Sí"
+                                value='yes'
+                                label='Sí'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="yes-3rd-trimester"
-                                label="Sí, último trimestre"
+                                value='yes-3rd-trimester'
+                                label='Sí, último trimestre'
                             />
                         </YStack>
                     </RadioGroup>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-parasite"
-                        onValueChange={handleChange('radio-parasite')}
+                        aria-labelledby='Select one item'
+                        name='radio-parasites'
+                        onValueChange={handleChange('radio-parasites')}
                     >
                         <H4>{'Parásitos Externos'}</H4>
                         <YStack
@@ -162,19 +178,19 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="1"
-                                label="Garrapatas"
+                                value='ticks'
+                                label='Garrapatas'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="2"
-                                label="Piojos"
+                                value='lices'
+                                label='Piojos'
                             />
                         </YStack>
                     </RadioGroup>
                     <RadioGroup
-                        aria-labelledby="Select one item"
-                        name="radio-mange"
+                        aria-labelledby='Select one item'
+                        name='radio-mange'
                         onValueChange={handleChange('radio-mange')}
                     >
                         <H4>{'Sarna'}</H4>
@@ -184,54 +200,57 @@ const Form10: FC<WrappedFormProps> = ({
                         >
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="light"
-                                label="Leve"
+                                value='light'
+                                label='Leve'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="moderate"
-                                label="Moderado"
+                                value='moderate'
+                                label='Moderado'
                             />
                             <RadioGroupItem
                                 size={dimensions.radioSize}
-                                value="severe"
-                                label="Severo"
+                                value='severe'
+                                label='Severo'
                             />
                         </YStack>
                     </RadioGroup>
                     <H4>{'Caspa'}</H4>
-                    <XStack alignItems='center' space={dimensions.checkboxSpacing}>
-                        <Checkbox id={'chckbx-dandruff'} size={dimensions.checkboxSize} >
-                            <Checkbox.Indicator>
-                                <CheckIcon />
-                            </Checkbox.Indicator>
-                        </Checkbox>
-                        <Label size={dimensions.checkboxLabelSize} htmlFor={'chckbx-dandruff'}>
-                            {`Tiene caspa`}
-                        </Label>
-                    </XStack>
+                    <CheckboxInput
+                        id='chkbx-has-dandruff'
+                        name='chkbx-has-dandruff'
+                        label='Tiene caspa'
+                        onChange={() => handleChange('chkbx-has-dandruff')}
+                        size={{
+                            label: dimensions.checkboxLabelSize,
+                            checkBox: dimensions.checkboxSize,
+                            spacing: dimensions.checkboxSpacing,
+                        }}
+                    />
                     <H4>{'Animal muerto'}</H4>
-                    <XStack alignItems='center' space={dimensions.checkboxSpacing}>
-                        <Checkbox id={'chckbx-is-dead'} size={dimensions.checkboxSize} >
-                            <Checkbox.Indicator>
-                                <CheckIcon />
-                            </Checkbox.Indicator>
-                        </Checkbox>
-                        <Label size={dimensions.checkboxLabelSize} htmlFor={'chckbx-is-dead'}>
-                            {`Esta muerto`}
-                        </Label>
-                    </XStack>
+                    <CheckboxInput
+                        id='chckbx-is-dead'
+                        name='chckbx-is-dead'
+                        label='Está muerto'
+                        onChange={() => handleChange('chckbx-is-dead')}
+                        size={{
+                            label: dimensions.checkboxLabelSize,
+                            checkBox: dimensions.checkboxSize,
+                            spacing: dimensions.checkboxSpacing,
+                        }}
+                    />
                     <H4>{'Esquila'}</H4>
-                    <XStack alignItems='center' space={dimensions.checkboxSpacing}>
-                        <Checkbox id={'chckbx-can-be-sheared'} size={dimensions.checkboxSize} >
-                            <Checkbox.Indicator>
-                                <CheckIcon />
-                            </Checkbox.Indicator>
-                        </Checkbox>
-                        <Label size={dimensions.checkboxLabelSize} htmlFor={'chckbx-can-be-sheared'}>
-                            {`Se esquila`}
-                        </Label>
-                    </XStack>
+                    <CheckboxInput
+                        id='chckbx-can-be-sheared'
+                        name='chckbx-can-be-sheared'
+                        label='Se esquila'
+                        onChange={() => handleChange('chckbx-can-be-sheared')}
+                        size={{
+                            label: dimensions.checkboxLabelSize,
+                            checkBox: dimensions.checkboxSize,
+                            spacing: dimensions.checkboxSpacing,
+                        }}
+                    />
                     <>
                         <H4>{'Observaciones'}</H4>
                         <TextArea
