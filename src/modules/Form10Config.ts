c43268be82fe987504c6b774fc85ec138da4d104
iamdevdaniel <maydanachi@gmail.com>
2023-12-27 19:23:03 -0400

    Implemented the validation of all fields in form10header except for the time picker

--- src/modules/Form10Config.ts
+++ src/modules/Form10Config.ts
@@ -1,6 +1,29 @@
+import { isInteger } from 'formik'
 import { object, string } from 'yup'
 
 import { isGreaterThanMin, isLessThanMax } from '../utilities/helpers'
+
+const range = {
+    length: { min: 1, max: 15 },
+    weight: { min: 10, max: 100 },
+    captureSiteNameChars: { max: 50 },
+}
+
+const errors = {
+    isSingleOption: 'Escoga una opción',
+    isRequired: 'Este campo es requerido',
+    maxCharLength: (max: number) => `${max} carácteres como máximo`,
+    shouldBeGreaterThan: (min: number) => `Debe ser mayor que ${min}`,
+    isNotANumber: 'Debe ser un número',
+    isCoordinate: 'Debe ser una coordenada válida',
+    isInteger: 'Debe ser un número entero',
+}
+
+const regex = {
+    coordinates: (value: string | undefined) =>
+        value !== undefined &&
+        /^-?([1-8]?[1-9]|[1-9]0)\.{1}\d{2,6}$/.test(value),
+}
 
 export const initialValuesForm10Entry = {
     'chbx-parasites': [
@@ -16,10 +39,52 @@ export const initialValuesForm10Entry = {
     'text-live-weight': '',
 }
 
-const range = {
-    length: { min: 1, max: 15 },
-    weight: { min: 10, max: 100 },
+export const initialValuesForm10Header = {
+    department: '',
+    regional: '',
+    community: '',
+    captureSite: '',
+    latitude: '',
+    longitude: '',
+    captureDate: null,
+    herdingAttempts: '',
+    authorizationCode: '',
 }
+
+export const validationSchemaForm10Header = object().shape({
+    department: string().required(errors.isSingleOption),
+    regional: string().required(errors.isSingleOption),
+    community: string().required(errors.isSingleOption),
+    captureSite: string()
+        .test('isRequired', errors.isRequired, value => !!value)
+        .test(
+            'maxLength50',
+            errors.maxCharLength(range.captureSiteNameChars.max),
+            value => !!value && value.length <= range.captureSiteNameChars.max,
+        ),
+    latitude: string()
+        .test('isRequired', errors.isRequired, value => !!value)
+        .test('isNumber', errors.isNotANumber, value => !isNaN(Number(value)))
+        .test('isCoordinate', errors.isCoordinate, value =>
+            regex.coordinates(value),
+        ),
+    longitude: string()
+        .test('isRequired', errors.isRequired, value => !!value)
+        .test('isNumber', errors.isNotANumber, value => !isNaN(Number(value)))
+        .test('isCoordinate', errors.isCoordinate, value =>
+            regex.coordinates(value),
+        ),
+    captureDate: string().required(errors.isRequired),
+    herdingAttempts: string()
+        .test('isNotEmpty', errors.isRequired, value => !!value)
+        .test('isNumber', errors.isNotANumber, value => !isNaN(Number(value)))
+        .test(
+            'greaterThanOne',
+            errors.shouldBeGreaterThan(0),
+            value => Number(value) > 1,
+        ),
+    authorizationCode: string().required(errors.isRequired),
+})
 
 export const validationSchemaForm10Entry = object().shape({
     'radio-age': string().required('Escoga una opción'),
@@ -38,7 +103,7 @@ export const validationSchemaForm10Entry = object().shape({
             isGreaterThanMin(range.length.min),
         )
         .test(
-            'maxLength',
+            'maxCharLength',
             `La longitud debe ser menor que ${range.length.max} cm`,
             isLessThanMax(range.length.max),
         )
--- src/modules/Form10Header.tsx
+++ src/modules/Form10Header.tsx
@@ -1,26 +1,35 @@
 import {
+    Input,
     IndexPath,
-    Button
+    Text,
+    Button,
+    Datepicker,
+    useTheme,
 } from '@ui-kitten/components'
 import { Formik } from 'formik'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { date } from 'yup'
 
 import ExtendedSelect from '../components/ExtendedSelect'
 import { getListOf } from '../models/arcmv'
 
+import {
+    initialValuesForm10Header,
+    validationSchemaForm10Header,
+} from './Form10Config'
+
 const Form10Header: React.FC = () => {
+    const onSubmit = (values: unknown) => console.log(values)
+    const theme = useTheme()
 
     return (
         <Formik
-            initialValues={{
-                department: '',
-                regional: '',
-                community: '',
-            }}
-            onSubmit={() => { }}
+            initialValues={initialValuesForm10Header}
+            validationSchema={validationSchemaForm10Header}
+            onSubmit={onSubmit}
         >
-            {({ values, setFieldValue }) => {
+            {({ values, isValid, dirty, setFieldValue, handleSubmit }) => {
                 const departments = getListOf.departments()
 
                 const regionals = React.useMemo(() => {
@@ -43,6 +52,7 @@ const Form10Header: React.FC = () => {
                 return (
                     <View style={styles.container}>
                         <ExtendedSelect
+                            style={styles.field}
                             label={'Departamento'}
                             placeholder={'Seleccione una opción'}
                             value={values.department}
@@ -57,6 +67,7 @@ const Form10Header: React.FC = () => {
                             }}
                         />
                         <ExtendedSelect
+                            style={styles.field}
                             label={'Asociación Regional'}
                             placeholder={'Seleccione una opción'}
                             value={values.regional}
@@ -71,6 +82,7 @@ const Form10Header: React.FC = () => {
                             }}
                         />
                         <ExtendedSelect
+                            style={styles.field}
                             label={'Comunidad Manejadora'}
                             placeholder={'Seleccione una opción'}
                             value={values.community}
@@ -83,7 +95,94 @@ const Form10Header: React.FC = () => {
                                 )
                             }}
                         />
-                        <Button onPress={() => { }}>Guardar</Button>
+                        <View style={styles.field}>
+                            <Text
+                                style={[
+                                    styles.subtitle,
+                                    { color: theme['color-basic-600'] },
+                                ]}
+                                category="label"
+                            >
+                                {'Coordenadas'}
+                            </Text>
+
+                            <View style={styles.coordinates}>
+                                <Input
+                                    style={[
+                                        styles.coordinatesInput,
+                                        styles.firstCoordinate,
+                                    ]}
+                                    label={'Latitud'}
+                                    value={values.latitude}
+                                    onChange={event => {
+                                        setFieldValue(
+                                            'latitude',
+                                            event.nativeEvent.text,
+                                        )
+                                    }}
+                                />
+                                <Input
+                                    style={styles.coordinatesInput}
+                                    label={'Longitud'}
+                                    value={values.longitude}
+                                    onChange={event => {
+                                        setFieldValue(
+                                            'longitude',
+                                            event.nativeEvent.text,
+                                        )
+                                    }}
+                                />
+                            </View>
+                        </View>
+                        <Input
+                            style={styles.field}
+                            label={'Sitio de captura'}
+                            value={values.captureSite}
+                            onChange={event => {
+                                setFieldValue(
+                                    'captureSite',
+                                    event.nativeEvent.text,
+                                )
+                            }}
+                        />
+                        <Datepicker
+                            style={styles.field}
+                            label={'Fecha de captura'}
+                            date={values.captureDate}
+                            placement={'left end'}
+                            onSelect={date =>
+                                setFieldValue('captureDate', date)
+                            }
+                        />
+                        <Input
+                            style={styles.field}
+                            label={'Numero de repeticiones de arreo'}
+                            value={values.herdingAttempts}
+                            onChange={event =>
+                                setFieldValue(
+                                    'herdingAttempts',
+                                    event.nativeEvent.text,
+                                )
+                            }
+                        />
+                        <Input
+                            style={styles.field}
+                            label={'Codigo de autorización de esquila'}
+                            value={values.authorizationCode}
+                            onChange={event =>
+                                setFieldValue(
+                                    'authorizationCode',
+                                    event.nativeEvent.text,
+                                )
+                            }
+                        />
+                        <Button
+                            style={styles.button}
+                            disabled={!isValid || !dirty}
+                            onPress={() => handleSubmit()}
+                        >
+                            Guardar
+                        </Button>
                     </View>
                 )
             }}
@@ -96,5 +195,23 @@ export default Form10Header
 const styles = StyleSheet.create({
     container: {
         width: '100%',
+    },
+    field: {
+        marginBottom: 8,
+    },
+    subtitle: {
+        marginBottom: 8,
+    },
+    coordinates: {
+        flexDirection: 'row',
+    },
+    coordinatesInput: {
+        flex: 1,
+    },
+    firstCoordinate: {
+        marginRight: 8,
+    },
+    button: {
+        marginTop: 8,
     },
 })
