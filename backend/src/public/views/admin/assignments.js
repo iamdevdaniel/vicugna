59d0ad97169e69193fdec350772eb09398f703e4
Daniel Maydana <maydanachi@gmail.com>
2026-07-10 05:11:42 -0400

    chore(assignment state): remove unused property

--- backend/src/public/views/admin/assignments.js
+++ backend/src/public/views/admin/assignments.js
@@ -14,7 +14,6 @@ function assignmentSeasonState(initialData) {
 		isUserPickerOpen: false,
 		userSearch: "",
 		selectedUserId: "",
-		selectedUserName: "",
 		get selectedPermit() {
 			const permit = this.permits.find(
 				(currentPermit) => currentPermit.id === this.selectedPermitId,
@@ -114,13 +113,11 @@ function assignmentSeasonState(initialData) {
 		},
 		selectUser(user) {
 			this.selectedUserId = user.id
-			this.selectedUserName = user.name
 			this.userSearch = user.name
 			this.isUserPickerOpen = false
 		},
 		clearUser() {
 			this.selectedUserId = ""
-			this.selectedUserName = ""
 			this.userSearch = ""
 			this.isUserPickerOpen = false
 		},
